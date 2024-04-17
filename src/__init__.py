75da042a6708f61f833601b621215eb0e3590cde
Kristoffer Hemm <kristoffer.hemm@gmail.com>
2024-04-17 11:44:30 +0200

    removed src

--- src/__init__.py
+++
@@ -1,3 +0,0 @@
-__version__ = "1.1.0"
-__author__ = "Kristoffer Hemm / NMBU, Nissan Karki / NMBU"
-__email__ = "kristoffer.hemm@nmbu.no, nissan.karki@nmbu.no"
--- src/custom_exceptions.py
+++
@@ -1,2 +0,0 @@
-class IllegalActionError(Exception):
-    pass
